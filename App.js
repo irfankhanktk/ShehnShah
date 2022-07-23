5878d0751d5e9474316086ce584dfe69e8b3cbb2
bilalqureshi <bilalqureshi1390@gmail.com>
2022-07-23 17:22:00 +0500

    api start

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
-import React, { useState } from 'react';
+import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
+import React, {useState} from 'react';
 import DropdownAlert from 'react-native-dropdownalert';
-import { Provider } from 'react-redux';
-import { MainNavigator } from './src/navigation';
+import {Provider} from 'react-redux';
+import {MainNavigator} from './src/navigation';
 import './src/services/axios-interceptors';
 import SERVICES from './src/services/common-services';
 import palette from './src/services/palette';
 import store from './src/store';
+//import {store} from './src/Redux/Store';
+
 import ThemeContext from './src/context/theme-context';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 // const defaultMode = Appearance.getColorScheme() || 'light';
 const App = () => {
   // const [themeState, setThemeState] = useState(defaultMode);
@@ -18,28 +20,28 @@ const App = () => {
   const alertRef = React.useRef();
   const getData = async () => {
     try {
-      const custTheme = await AsyncStorage.getItem('@custTheme')
+      const custTheme = await AsyncStorage.getItem('@custTheme');
       if (custTheme != null) {
-        setThemeApplied(custTheme)
+        setThemeApplied(custTheme);
       } else {
         await AsyncStorage.setItem('@custTheme', 'light');
       }
     } catch (e) {
       // error reading value
     }
-  }
+  };
   const themeContext = React.useMemo(
     () => ({
-      toggleTheme: (color) => {
+      toggleTheme: color => {
         setThemeApplied(color);
         (async () => {
           await AsyncStorage.setItem('@custTheme', color);
-        })()
+        })();
       },
       showAlert: (type, error) => {
         alertRef.current.alertWithType(
-          (type || 'error'),
-          (SERVICES._capitalizeFirst(type) || 'Error'),
+          type || 'error',
+          SERVICES._capitalizeFirst(type) || 'Error',
           SERVICES._returnError(error),
         );
       },
@@ -53,7 +55,10 @@ const App = () => {
     })();
   }, []);
 
-  const theme = themeApplied === 'dark' ? { ...DefaultTheme.colors, ...palette.darkTheme } : { ...DefaultTheme.colors, ...palette.lightTheme }
+  const theme =
+    themeApplied === 'dark'
+      ? {...DefaultTheme.colors, ...palette.darkTheme}
+      : {...DefaultTheme.colors, ...palette.lightTheme};
   return (
     <SafeAreaProvider>
       <Provider store={store}>
--- src/screens/signin-screen/signin.js
+++ src/screens/signin-screen/signin.js
@@ -1,17 +1,19 @@
-import { useNavigation, useTheme } from '@react-navigation/native';
-import React,{useState,useRef} from 'react';
-import { ScrollView, View } from 'react-native';
-import { connect } from 'react-redux';
-import { Apple, Facebook, Google, SLogo, Tick } from '../../assets/common-icons';
+import {useNavigation, useTheme} from '@react-navigation/native';
+import React, {useState, useRef} from 'react';
+import {ScrollView, View} from 'react-native';
+import {connect} from 'react-redux';
+import {Apple, Facebook, Google, SLogo, Tick} from '../../assets/common-icons';
 import Buttons from '../../components/atoms/Button';
-import { INPUT_FIELD } from '../../components/atoms/Input';
+import {INPUT_FIELD} from '../../components/atoms/Input';
 import Bold from '../../presentation/typography/bold-text';
 import Regular from '../../presentation/typography/regular-text';
 import allColors from '../../services/colors';
-import { mvs } from '../../services/metrices';
+import {mvs} from '../../services/metrices';
 import DIVIY_API from '../../store/api-calls';
-import PhoneInput from 'react-native-phone-number-input'
-import { Signin_Styles as styles } from './signin-styles';
+import PhoneInput from 'react-native-phone-number-input';
+import {Signin_Styles as styles} from './signin-styles';
+import alertService from '../../services/alert.service';
+import {intValidation} from '../../services/validation';
 const Signin = props => {
   const navigation = useNavigation();
   const [loading, setLoading] = React.useState(false);
@@ -19,31 +21,39 @@ const Signin = props => {
   const [isSignUpWithPhone, setPhoneSignUp] = React.useState(true);
   const [phoneNumber, setphoneNumber] = useState('12015550123');
   const phoneInput = useRef(null);
-  const [formattedValue, setFormattedValue] = useState("");
+  const [formattedValue, setFormattedValue] = useState('');
   const [payload, setPayload] = React.useState({
     email: '',
     password: '',
-    name:'',
-    confirmPassword:''
+    name: '',
+    confirmPassword: '',
   });
-  const { colors } = useTheme();
+  const {colors} = useTheme();
 
   const onSigin = async () => {
-      setSelectedTab("login")
-   };
+    setSelectedTab('login');
+  };
   const onSigUp = async () => {
-    setSelectedTab("signup")
-    setPhoneSignUp(false)
+    setSelectedTab('signup');
+    setPhoneSignUp(false);
   };
   const onSigUpWithPhone = async () => {
-    setPhoneSignUp(true)
+    setPhoneSignUp(true);
   };
- 
+
+  const getMobile = async => {
+    if (formattedValue === '') {
+      return console.log('Plx Enter Number');
+    } else if (intValidation(formattedValue)) {
+      return console.log('false');
+    }
+    console.log('true');
+  };
   return (
-    <View style={{ ...styles.container, backgroundColor: colors.background }}>
+    <View style={{...styles.container, backgroundColor: colors.background}}>
       <ScrollView>
         <View style={styles.body}>
-           {/* <View style={{alignItems: 'center'}}>
+          {/* <View style={{alignItems: 'center'}}>
             <SLogo  />
           </View>
          <View style={styles.tabView}>
@@ -64,155 +74,183 @@ const Signin = props => {
               title={'Register '}
             />
           </View> */}
-          {selectedTab=="login"?
-          <>
-          <Bold label={"Welcome Back!"} style={styles.welcomeText}/>
-          <Regular label={"Please enter your email and password"} style={styles.welcomeSubText}/>
-          <Regular label={"to contiue"} style={styles.welcomeSubText}/>
-          <View style={styles.input_container}>
-            <INPUT_FIELD.InputSecondary
-              value={payload.email}
-                leftIcon='User'
-                rightIcon=''
-              onChangeText={t => setPayload({...payload, email: t})}
-              label="EMAIL"
-              placeholder="lehieuds@gmail.com"
-              
-            />
-            <INPUT_FIELD.InputSecondary
-              secureTextEntry
-                leftIcon='User'
-                rightIcon=''
-              value={payload.password}
-              onChangeText={t => setPayload({...payload, password: t})}
-              label="PASSWORD"
-              placeholder="Password"
-            />
-          </View>
-          <Bold label={"Forgot Password?"} style={styles.forgotText}/>
-          <Buttons.ButtonPrimary
-            disabled={loading}
-            loading={loading}
-            onClick={onSigin}
-            textStyle={styles.buttonText}
-            style={{...styles.button}}
-            title={'Login'}
-          />
-          <Regular label={"Continue With"} style={styles.continueWithText}/>
-          <View style={styles.socialIconView}>
-              <Google/>
-              <Regular label={"Sign in with Google"} style={styles.socialIconText}/>
-          </View>
-          <View style={styles.socialIconView}>
-              <Facebook/>
-              <Regular label={"Sign in with Facebook"} style={styles.socialIconText}/>
-          </View>
-          <View style={styles.socialIconView}>
-              <Apple/>
-              <Regular label={"Sign in with Facebook"} style={styles.socialIconText}/>
-          </View>
-          </>:selectedTab=="signup" && isSignUpWithPhone==false?
-          <>
-          <Bold label={"Create Your Account"} style={styles.welcomeText}/>
-          <Regular label={"Enter your full name and email address to"} style={styles.welcomeSubText}/>
-          <Regular label={"create your account"} style={styles.welcomeSubText}/>
-          
-          <View style={styles.input_container}>
-
-          <INPUT_FIELD.InputSecondary
-              rightIcon={false}
-              leftIcon='User'
-              value={payload.name}
-              onChangeText={t => setPayload({...payload, name: t})}
-              label="FULL NAME"
-              placeholder="John Doe"
-              
-            />
-
-            <INPUT_FIELD.InputSecondary
-              value={payload.email}
-                leftIcon='User'
-                rightIcon='Tick'
-              onChangeText={t => setPayload({...payload, email: t})}
-              label="EMAIL"
-              placeholder="lehieuds@gmail.com"
-              
-            />
-            <INPUT_FIELD.InputSecondary
-              secureTextEntry
-              leftIcon='Lock'
-              value={payload.password}
-              onChangeText={t => setPayload({...payload, password: t})}
-              label="PASSWORD"
-              placeholder="Password"
-            />
-
-            <INPUT_FIELD.InputSecondary
-              secureTextEntry
-                leftIcon='User'
-                rightIcon='Lock'
-              value={payload.confirmPassword}
-              onChangeText={t => setPayload({...payload, confirmPassword: t})}
-              label="CONFIRM PASSWORD"
-              placeholder="Confirm Password"
-            />
-          </View>
-          <Buttons.ButtonPrimary
-            disabled={loading}
-            loading={loading}
-            onClick={onSigin}
-            textStyle={{...styles.buttonText,color:colors.white}}
-            style={{...styles.button}}
-            title={'Continue'}
-          />
-          <Buttons.ButtonPrimary
-            disabled={loading}
-            loading={loading}
-            onClick={onSigUpWithPhone}
-            textStyle={styles.buttonText}
-            style={{...styles.button,backgroundColor:colors.white,borderWidth:1.4,borderColor:allColors.primary}}
-            title={'Sign up with Phone Number'}
-          />
-
-          </>:isSignUpWithPhone==true?
-           <>
-            <Regular label={"Front Row"} style={styles.frontRowText}/>
-            <Bold label={"Welcome Back!"} style={styles.welcomeText}/>
-            <Regular label={"Enter your mobile number"} style={styles.welcomeSubText}/>
-            <Regular label={"to continue."} style={styles.welcomeSubText}/>
-            <Bold label={"MOBILE"} style={{marginTop:mvs(30)}}>
-              <Regular label={" NUMBER"}/>
-             </Bold>
-            <View style={{...styles.phoneNumberView,marginTop:mvs(10)}}>
-              <PhoneInput
-                    ref={phoneInput}
-                    defaultValue="(201) 555-0123"
-                    defaultCode="US"
-                    layout="first"
-                    containerStyle={styles.phoneContainer}
-                    textContainerStyle={styles.textInput}
-                    onChangeFormattedText={(text) => {
-                      setFormattedValue(text);
-                      console.log("Formated Value "+text)
-                    }}
-                    onChangeText={(text) => {
-                      setphoneNumber(text); }}
-                 />
-                <Tick style={{}}/>
-             </View>
+          {selectedTab == 'login' ? (
+            <>
+              <Bold label={'Welcome Back!'} style={styles.welcomeText} />
+              <Regular
+                label={'Please enter your email and password'}
+                style={styles.welcomeSubText}
+              />
+              <Regular label={'to contiue'} style={styles.welcomeSubText} />
+              <View style={styles.input_container}>
+                <INPUT_FIELD.InputSecondary
+                  value={payload.email}
+                  leftIcon="User"
+                  rightIcon=""
+                  onChangeText={t => setPayload({...payload, email: t})}
+                  label="EMAIL"
+                  placeholder="lehieuds@gmail.com"
+                />
+                <INPUT_FIELD.InputSecondary
+                  secureTextEntry
+                  leftIcon="User"
+                  rightIcon=""
+                  value={payload.password}
+                  onChangeText={t => setPayload({...payload, password: t})}
+                  label="PASSWORD"
+                  placeholder="Password"
+                />
+              </View>
+              <Bold label={'Forgot Password?'} style={styles.forgotText} />
               <Buttons.ButtonPrimary
                 disabled={loading}
                 loading={loading}
-                onClick={()=>navigation.navigate("Otp")}
-                textStyle={{...styles.buttonText,color:colors.white}}
+                onClick={onSigin}
+                textStyle={styles.buttonText}
+                style={{...styles.button}}
+                title={'Login'}
+              />
+              <Regular
+                label={'Continue With'}
+                style={styles.continueWithText}
+              />
+              <View style={styles.socialIconView}>
+                <Google />
+                <Regular
+                  label={'Sign in with Google'}
+                  style={styles.socialIconText}
+                />
+              </View>
+              <View style={styles.socialIconView}>
+                <Facebook />
+                <Regular
+                  label={'Sign in with Facebook'}
+                  style={styles.socialIconText}
+                />
+              </View>
+              <View style={styles.socialIconView}>
+                <Apple />
+                <Regular
+                  label={'Sign in with Facebook'}
+                  style={styles.socialIconText}
+                />
+              </View>
+            </>
+          ) : selectedTab == 'signup' && isSignUpWithPhone == false ? (
+            <>
+              <Bold label={'Create Your Account'} style={styles.welcomeText} />
+              <Regular
+                label={'Enter your full name and email address to'}
+                style={styles.welcomeSubText}
+              />
+              <Regular
+                label={'create your account'}
+                style={styles.welcomeSubText}
+              />
+
+              <View style={styles.input_container}>
+                <INPUT_FIELD.InputSecondary
+                  rightIcon={false}
+                  leftIcon="User"
+                  value={payload.name}
+                  onChangeText={t => setPayload({...payload, name: t})}
+                  label="FULL NAME"
+                  placeholder="John Doe"
+                />
+
+                <INPUT_FIELD.InputSecondary
+                  value={payload.email}
+                  leftIcon="User"
+                  rightIcon="Tick"
+                  onChangeText={t => setPayload({...payload, email: t})}
+                  label="EMAIL"
+                  placeholder="lehieuds@gmail.com"
+                />
+                <INPUT_FIELD.InputSecondary
+                  secureTextEntry
+                  leftIcon="Lock"
+                  value={payload.password}
+                  onChangeText={t => setPayload({...payload, password: t})}
+                  label="PASSWORD"
+                  placeholder="Password"
+                />
+
+                <INPUT_FIELD.InputSecondary
+                  secureTextEntry
+                  leftIcon="User"
+                  rightIcon="Lock"
+                  value={payload.confirmPassword}
+                  onChangeText={t =>
+                    setPayload({...payload, confirmPassword: t})
+                  }
+                  label="CONFIRM PASSWORD"
+                  placeholder="Confirm Password"
+                />
+              </View>
+              <Buttons.ButtonPrimary
+                disabled={loading}
+                loading={loading}
+                onClick={onSigin}
+                textStyle={{...styles.buttonText, color: colors.white}}
                 style={{...styles.button}}
                 title={'Continue'}
               />
-           </>:null
-           
-          }
+              <Buttons.ButtonPrimary
+                disabled={loading}
+                loading={loading}
+                onClick={onSigUpWithPhone}
+                textStyle={styles.buttonText}
+                style={{
+                  ...styles.button,
+                  backgroundColor: colors.white,
+                  borderWidth: 1.4,
+                  borderColor: allColors.primary,
+                }}
+                title={'Sign up with Phone Number'}
+              />
+            </>
+          ) : isSignUpWithPhone == true ? (
+            <>
+              <Regular label={'Front Row'} style={styles.frontRowText} />
+              <Bold label={'Welcome Back!'} style={styles.welcomeText} />
+              <Regular
+                label={'Enter your mobile number'}
+                style={styles.welcomeSubText}
+              />
+              <Regular label={'to continue.'} style={styles.welcomeSubText} />
+              <Bold label={'MOBILE'} style={{marginTop: mvs(30)}}>
+                <Regular label={' NUMBER'} />
+              </Bold>
+              <View style={{...styles.phoneNumberView, marginTop: mvs(10)}}>
+                <PhoneInput
+                  ref={phoneInput}
+                  // defaultValue="(201) 555-0123"
+                  defaultCode="US"
+                  layout="first"
+                  containerStyle={styles.phoneContainer}
+                  textContainerStyle={styles.textInput}
+                  onChangeFormattedText={text => {
+                    setFormattedValue(text);
+                    console.log('Formated Value ' + text);
+                  }}
+                  onChangeText={text => {
+                    setphoneNumber(text);
+                  }}
+                />
+                <Tick style={{}} />
+              </View>
+              <Buttons.ButtonPrimary
+                disabled={loading}
+                loading={loading}
+                // onClick={getMobile}
+                onClick={() => navigation.navigate('Otp')}
+                textStyle={{...styles.buttonText, color: colors.white}}
+                style={{...styles.button}}
+                title={'Continue'}
+              />
+            </>
+          ) : null}
         </View>
-
       </ScrollView>
     </View>
   );
